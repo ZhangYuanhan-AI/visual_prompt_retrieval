6a6ee7eb5fbbfc86c69da246a826ff471c374311
Davidzhangyuanhan <704464069@qq.com>
2023-01-05 15:41:48 +0800

    add supcon

--- tools/featextrater_imagenet_cont.py
+++ tools/featextrater_imagenet_cont.py
@@ -56,7 +56,7 @@ center_crop = T.Compose(t)
 #    sys.exit()
  
  
-save_dir = "/mnt/lustre/yhzhang/data/imagenet/features_supcon_bs64_val"
+save_dir = "/mnt/lustre/yhzhang/data/imagenet/features_supcon-in1k-pretrain_val"
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 else:
@@ -64,7 +64,8 @@ else:
     sys.exit()
 
 model = SupConResNet('resnet50')
-model.load_state_dict(clean_state_dict(torch.load('/mnt/lustre/yhzhang/SupContrast/save/SupCon/path_models/SupCon_path_resnet50_seed_0_lr_0.005_decay_0.0001_cropsz_224_bsz_64_temp_0.1_trial_0_cosine_pretrain/last.pth')['model']))
+model.load_state_dict(clean_state_dict(torch.load('/mnt/lustre/yhzhang/SupContrast/weights/supcon.pth')['model_ema']))
+# model.load_state_dict(clean_state_dict(torch.load('/mnt/lustre/yhzhang/SupContrast/save/SupCon/path_models/SupCon_path_resnet50_seed_0_lr_0.005_decay_0.0001_cropsz_224_bsz_64_temp_0.1_trial_0_cosine_pretrain/last.pth')['model']))
 model.eval()
 model = model.cuda()
 
