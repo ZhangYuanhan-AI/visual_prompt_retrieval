b3f7c86b802d9186713ec83878ce7b3322823122
Davidzhangyuanhan <704464069@qq.com>
2023-01-06 15:04:41 +0800

    updates

--- evaluate/srun_color_evaluate.sh
+++ evaluate/srun_color_evaluate.sh
@@ -8,7 +8,7 @@ currenttime=`date "+%Y%m%d_%H%M%S"`
 # do
 export MASTER_PORT=$((12000 + $RANDOM % 20000))
 srun -p ntu --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 -w SG-IDC2-10-51-5-46 \
-python evaluate_colorization.py --model mae_vit_large_patch16 --output_dir output_color_images/output_supcon-in1k-pretrain_val --feature_name features_supcon-in1k-pretrain_val --ckpt ../weights/checkpoint-1000.pth --data_path /mnt/lustre/yhzhang/data/imagenet \
+python evaluate_colorization.py --model mae_vit_large_patch16 --output_dir output_color_images/output_supcon-vit-laion2b-freeze-encoder_val --feature_name features_vit-laion2b-freeze-encoder_val --ckpt ../weights/checkpoint-1000.pth --data_path /mnt/lustre/yhzhang/data/imagenet \
 2>&1 | tee -a logs/${currenttime}.log > /dev/null &
 echo -e "\033[32m[ Please check log: \"logs/${currenttime}.log\" for details. ]\033[0m"
 # done
